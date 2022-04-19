e8bd6c5f8e7107b17b0bff21860ccf2fb0615961
gedunlap <g.e.dunlap90@gmail.com>
2022-04-19 17:15:21 -0400

    navbar mod

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoLogoInstagram } from 'react-icons/io'
 import { IoMailOutline } from 'react-icons/io5'
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnA, ShopNav } from './NavbarElements'
+import { Nav, NavLink, Bars, NavMenu, NavBtn} from './NavbarElements'
 
 const Navbar = ({toggle}) => {
   return (
@@ -16,21 +16,15 @@ const Navbar = ({toggle}) => {
                 About
               </NavLink>
               <NavLink to="/gallery">
-                Artists & Gallery
+                Gallery
               </NavLink>
               <NavLink to="/services">
                 Services
               </NavLink>
-              <ShopNav>
-                <a href="https://www.fresha.com/store/material-t8mbqg4o" target="_blank" style={{textDecoration:"none", color:"black"}}>Shop</a>
-              </ShopNav>
             </NavMenu>
           </div>
           <Bars onClick={toggle} />
           <NavBtn>
-            <NavBtnA>
-              <a href="https://www.fresha.com/a/material-tampa-2502-north-howard-avenue-tjr5uxjo/booking?menu=true" target="_blank" style={{textDecoration:"none", color:"white"}}>APPOINTMENTS</a>
-            </NavBtnA>
             <a href="https://www.instagram.com/materialtampa/" target="_blank"><IoLogoInstagram size='30' style={{color:"black"}} /></a>
             <a href="mailto: contact@materialtampa.com"><IoMailOutline size='30' style={{color:"black"}} /></a>
           </NavBtn>
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -17,20 +17,15 @@ const Sidebar = ({isOpen, toggle}) => {
               About
             </SidebarLink>
             <SidebarLink to='/gallery'>
-              Artists & Gallery
+              Gallery
             </SidebarLink>
             <SidebarLink to='/services'>
               Services
             </SidebarLink>
-            <ShopNav>
-              <a href="https://www.fresha.com/store/material-t8mbqg4o" target="_blank" style={{textDecoration:"none", color:"white"}}>Shop</a>
-            </ShopNav>
           </SidebarMenu>
         </SidebarWrapper>
         <SideBtnWrap>
-          <SidebarBtn>
-            <a className="sidebtn" href="https://www.fresha.com/a/material-tampa-2502-north-howard-avenue-tjr5uxjo/booking?menu=true" target="_blank" style={{textDecoration:"none", color:"black"}}>APPOINTMENTS</a>
-          </SidebarBtn>
+
         </SideBtnWrap>
     </SidebarContainer>
   )
