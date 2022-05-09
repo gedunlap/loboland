bc29e06db914e60cf5e3f4d152685a3e2e6ad8c8
gedunlap <g.e.dunlap90@gmail.com>
2022-05-09 19:49:35 -0400

    page routes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import Gallery from '../Pages/Gallery'
+import Services from '../Pages/Services'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className='main'>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </div>
   )
 }
 
